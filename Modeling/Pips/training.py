3d7b70f9a0cb068cadb693f248a97f26387123fd
G-IV <unccmarine@gmail.com>
2026-02-07 13:25:05 -0600

    Just a bunch of minor changes

--- Modeling/Pips/training.py
+++ Modeling/Pips/training.py
@@ -1,16 +1,16 @@
 from ultralytics import YOLO
 
 # Get absolute path for your data/model
-data_path = '/Users/georgeburrows/Documents/Desktop/Projects/Die Tester/Dice_Tester/Modeling/Cross_Bars/data.yaml'
+data_path = '/Users/georgeburrows/Documents/Desktop/Projects/Die Tester/Dice_Tester/Modeling/Pips/data.yaml'
 
 # Loads a model - using the absolute path, I can put the model where I want it.
-model = YOLO('/Users/georgeburrows/Documents/Desktop/Projects/Die Tester/Dice_Tester/Modeling/Cross_Bars/3_YOLO/yolov8n.pt')
+model = YOLO('/Users/georgeburrows/Documents/Desktop/Projects/Die Tester/Dice_Tester/Modeling/Pips/3_YOLO/yolov8n.pt')
 
 results = model.train(
     data=data_path, 
     epochs=100, 
     imgsz=1024, 
-    project='/Users/georgeburrows/Documents/Desktop/Projects/Die Tester/Dice_Tester/Modeling/Cross_Bars/3_YOLO/runs/', 
+    project='/Users/georgeburrows/Documents/Desktop/Projects/Die Tester/Dice_Tester/Modeling/Pips/3_YOLO/runs', 
     name='training',
     device="mps", 
     visualize=True,
