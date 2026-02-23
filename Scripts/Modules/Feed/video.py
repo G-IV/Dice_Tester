115fc9da75423f3e9326cebb29ea4bb3d23b9897
G-IV <unccmarine@gmail.com>
2026-02-23 14:49:18 -0600

    the straight through script with classes seems to be working.

--- Scripts/Modules/Feed/video.py
+++ Scripts/Modules/Feed/video.py
@@ -14,7 +14,8 @@ class Feed(feed.Feed):
             self,
             annotator: annotate.Annotator,
             data: project_data.ProjectData,
-            source: int = 0,
+            video_path: Path, 
+            auto_play: bool = True,
             logging: bool = False,
         ) -> None:
         super().__init__(
@@ -22,24 +23,25 @@ class Feed(feed.Feed):
             data=data, 
             logging=logging
         )
+        self.auto_play = auto_play
         self.cap = None
+        self.video_path = video_path
         self.processed_frames: list = []
-        self.open_source(source)
+        self.open_source()
         
         if self.logging:
             print(f"Initialized Video Feed with video path: {self.video_path}")
 
-    def open_source(self, source: int):
-
+    def open_source(self):
         """Open feed to video file"""
-        # AVFoundation is the default backend for macOS, and it supports a wide range of video formats. If you encounter issues with specific formats, you may need to install additional codecs or use a different backend.
-        self.cap = cv2.VideoCapture(source, cv2.CAP_AVFOUNDATION)
+        self.cap = cv2.VideoCapture(str(self.video_path))
         if not self.cap.isOpened():
-            raise ValueError(f"Could not open video source: {source}")
+            raise ValueError(f"Could not open video source: {self.video_path}")
+        self.frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        self.current_frame_index = 0
         self.fps = self.cap.get(cv2.CAP_PROP_FPS)
-        self.clear_frame_buffer()  # Clear the buffer to start with the most recent frame
         if self.logging:
-            print(f"Opened video source: {source}")
+            print(f"Opened video source: {self.video_path}")
 
     def capture_frame(self):
         """Capture a frame from the video feed."""
@@ -51,14 +53,6 @@ class Feed(feed.Feed):
         if not ret:
             raise ValueError("Could not read frame from video source.")
         
-    def clear_frame_buffer(self):
-        """Clear the frame buffer to ensure that we are processing the most recent frame."""
-        if self.cap is None:
-            raise ValueError("Video source is not opened.")
-        # Clear the buffer by reading frames until we get to the most recent one
-        for _ in range(5):  # Read a few frames to clear the buffer
-            self.cap.read()  # Grab the frame without decoding it
- 
     def perf_counter(self):
         """In lieu of importing time everywhere I need this for managing when capture_frame is called, I'll just add this as a function call"""
         return time.perf_counter()
